11e3afaadbeb49d2b5c4cbbcb696ebbfe7865fa7
P1cq <medoayman046@gmail.com>
2025-11-25 13:17:03 +0200

    creat repo and add README

---
+++ javascript.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// console.log('hello');
+// console.log('ssss');
+// console.log('sssss');
+// console.log('aaaa');
+
+var x;
+console.log(x);
+var g = 0;
+console.log(typeof g);
+var x = 3;
+console.log(x);
+
+console.log(x);
